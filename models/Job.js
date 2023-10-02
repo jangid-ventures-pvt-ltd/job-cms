f2b19f0d81aa9fecbeaf26ba5066afe20634f19c
Jayesh <jangidventurespvtltd@gmail.com>
2023-10-02 19:08:36 +0530

    sub test

--- models/Job.js
+++ models/Job.js
@@ -10,12 +10,13 @@ const jobSchema = new mongoose.Schema({
   salary: String,
   role: String,
   jobCategory: String,
+  subCategories: [String], // Add subCategories field as an array of strings
   posts: String,
-  jobOpeningDate: String,
-  jobClosingDate: String,
-  jobId: String,
+  jobOpeningDate: String, // Change to String
+  jobClosingDate: String, // Change to String
+  jobId: String, // Remove "required" validation
   aboutJob: String,
-  seoUrl: String,
+  seoUrl: String, // Change to String and remove "required" validation
   createdBy: String,
 }, {
   timestamps: {
